4c33c789bd6b5701de6a9f623df2163cd0102bd0
Victor Farias <victor.f.farias@gmail.com>
2020-07-29 21:05:35 -0300

    [FEAT] Creating basic form

--- src/pages/cadastro/Categoria/index.tsx
+++ src/pages/cadastro/Categoria/index.tsx
@@ -7,6 +7,18 @@ const CadastroCategoria: React.FC = () => {
         <PageDefault>
             <h1>Cadastro de Categoria</h1>
 
+            <form>
+                <label>
+                    Nome da Categoria:
+                    <input type="text" />
+                </label>
+
+            <button>
+                Cadastrar
+            </button>
+
+            </form>
+
             <Link to="/">
                 Ir para Home
             </Link>
